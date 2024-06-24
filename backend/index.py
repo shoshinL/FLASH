3fbaf5383dd15d48ea540e96048a7e4a88f047d9
shoshinL <linus.a.schneider@protonmail.com>
2024-06-24 22:51:50 +0200

    CHANGE: Turned of logging into file

--- backend/index.py
+++ backend/index.py
@@ -13,7 +13,8 @@ from agents.note_graph import graph
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler("debug.log"), logging.StreamHandler(sys.stdout)])
+                    #handlers=[logging.FileHandler("debug.log"), logging.StreamHandler(sys.stdout)])
+                    handlers=[logging.StreamHandler(sys.stdout)])
 logger = logging.getLogger(__name__)
 
 
